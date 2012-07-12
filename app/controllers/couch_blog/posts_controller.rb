bb422fc885595778bdc60e6b024e94f8464af131
Benjamin ter Kuile <bterkuile@gmail.com>
2012-07-12 17:37:00 +0200

    allow render params setting by engine user

--- app/controllers/couch_blog/posts_controller.rb
+++ app/controllers/couch_blog/posts_controller.rb
@@ -4,11 +4,13 @@ module CouchBlog
     # GET posts
     def index
       @posts = CouchBlog::Post.all
+      render couch_blog_render if defined? couch_blog_render
     end
 
     #GET posts/:id(/:title)
     def show
       @post = CouchBlog::Post.find(params[:id])
+      render couch_blog_render if defined? couch_blog_render
     end
   end
 end
